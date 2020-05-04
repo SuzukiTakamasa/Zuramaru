cda0196401a12215bb05a8d565021bf653e04475
Suzuki Takamasa <incubus.appalachia@gmail.com>
2020-05-04 15:31:42 +0900

    commit (include "forms.py")

--- miraizura/urls.py
+++ miraizura/urls.py
@@ -7,4 +7,5 @@ from . import views
 app_name = 'miraizura'
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
+    path('inquiry/', views.InquiryView.as_view(), name="inquiry")
 ]
