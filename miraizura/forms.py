fbc3181f8a6a18d13fdf5d5b6ce2914430492c68
Suzuki Takamasa <incubus.appalachia@gmail.com>
2020-05-04 18:28:52 +0900

    commit

--- miraizura/forms.py
+++ miraizura/forms.py
@@ -1,5 +1,6 @@
 
 from django import forms
+from django.core.mail import EmailMessage
 
 class InquiryForm(forms.Form):
     name = forms.CharField(label='Name', max_length=30)
@@ -21,3 +22,18 @@ class InquiryForm(forms.Form):
 
         self.fields['message'].widget.attrs['class'] = 'form-control col12'
         self.fields['message'].widget.attrs['placeholder'] = 'Enter a message'
+
+    def send_email(self):
+        name = self.cleaned_data['name']
+        email = self.cleaned_data['email']
+        title = self.cleaned_data['title']
+        message = self.cleaned_data['message']
+
+        subject = "Inquiry {}".format(title)
+        message = "Sender's name: {0}\nEmail address: {1}\nMessage:\n{2}".format(name, email, message)
+        from_email = 'admin@example.com'
+        to_list = ['test@example.com']
+        cc_list = [email]
+
+        message = EmailMessage(subject=subject, body=message, from_email=from_email, to=to_list, cc=cc_list)
+        message.send()
