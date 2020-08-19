e25f7f6ff4b4022ce87d8aa994dc54c5eebf5aa6
Suzuki Takamasa <incubus.appalachia@gmail.com>
2020-08-19 17:40:16 +0900

    commit for production env.

---
+++ zuramaru/settings.py
@@ -0,0 +1,56 @@
+from .settings_common import *
+
+#Whether to validate DEBUG mode
+DEBUG = False
+
+#The list of host name to be permitted
+ALLOWED_HOST = [os.environ.get('ALLOWED_HOSTS')]
+
+#The directory of locating static files
+STATIC_ROOT = '/usr/share/nginx/html/static'
+MEDIA_ROOT = '/usr/share/nginx/html/media'
+
+#The setting related to Amazon SES
+AWS_SES_ACCESS_KEY_ID = os.environ.get('AWS_SES_ACCESS_KEY_ID')
+AWS_SES_SECRET_ACCESS_KEY = os.environ.get('AWS_SES_SECRET_ACCESS_KEY')
+EMAIL_BACKEND = 'django_ses.SESBackend'
+
+#The setting of logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+        },
+        'diary': {
+            'handlers': ['file'],
+            'level': 'INFO',
+        },
+    },
+
+    'handlers': {
+         'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/django.log'),
+            'formatter': 'prod',
+            'when': 'D',
+            'interval': 1,
+            'backupCount': 7,
+         },
+    },
+
+    'formatters': {
+        'prod': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(Lineno)d)',
+                '%(message)s'
+            ])
+        },
+    }
+}
