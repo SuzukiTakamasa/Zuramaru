d07a498a65cd0c62a2d33c2e43e3a3c63695502c
Suzuki Takamasa <incubus.appalachia@gmail.com>
2020-09-09 21:49:38 +0900

    commit pillow

--- miraizura/models.py
+++ miraizura/models.py
@@ -6,6 +6,9 @@ class Diary(models.Model):
     user = models.ForeignKey(CustomUser, verbose_name='user', on_delete=models.PROTECT)
     title = models.CharField(verbose_name='title', max_length=40)
     content = models.TextField(verbose_name='content', blank=True, null=True)
+    photo1 = models.ImageField(verbose_name='photo1', blank=True, null=True)
+    photo2 = models.ImageField(verbose_name='photo2', blank=True, null=True)
+    photo3 = models.ImageField(verbose_name='photo3', blank=True, null=True)
     created_at = models.DateTimeField(verbose_name='created date', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='updated date', auto_now=True)
 
