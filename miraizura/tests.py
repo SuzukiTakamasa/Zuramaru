476c4cc61ac5f4034a1ff961a4e14679eb767f09
Suzuki Takamasa <incubus.appalachia@gmail.com>
2020-09-29 21:33:54 +0900

    commit accounts/tests.py revision

--- miraizura/tests.py
+++ miraizura/tests.py
@@ -13,13 +13,3 @@ class TestLogin(LiveServerTestCase):
         cls.selenium.quit()
         super().tearDownClass()
 
-    def test_login(self):
-        self.selenium.get('http://localhost:8000' + str(reverse_lazy('account_login')))
-
-        username_input = self.selenium.find_element_by_name("login")
-        username_input.send_keys('')
-        password_input = self.selenium.find_element_by_name("password")
-        password_input.send_keys('')
-        self.selenium.find_element_by_class_name('btn').click()
-
-        self.assertEquals('DIARY LIST | Zuramaru', self.selenium.title)
